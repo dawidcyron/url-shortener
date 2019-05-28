851c582fb735a9754cd401725fe8c76a4adcabb9
Dawid Cyron <dawidcyron@op.pl>
2019-05-28 12:35:37 +0200

    Allow Redis connection config with env variables

--- database/redis.go
+++ database/redis.go
@@ -2,14 +2,20 @@ package database
 
 import (
 	"log"
+	"os"
 
 	"github.com/go-redis/redis"
 )
 
 var RedisClient *redis.Client
 
+// NewRedisClient initiates Redis client connection
 func NewRedisClient() {
-	RedisClient = redis.NewClient(&redis.Options{})
+	RedisClient = redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_ADDR"),
+		Password: os.Getenv("REDIS_PASS"),
+		DB:       0,
+	})
 	_, err := RedisClient.Ping().Result()
 	if err != nil {
 		log.Fatal(err.Error())
